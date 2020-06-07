f6c76c80cf0e4614f3969d6c5316a42425e02eb6
Belkhir Nacim <belkhir.nacim@gmail.com>
2020-06-07 23:09:25 +0200

    reformat vqvae to have multiple input channel

--- generative_models_toolbox/algos/vqvae/vqvae.py
+++ generative_models_toolbox/algos/vqvae/vqvae.py
@@ -20,6 +20,7 @@ class VectorQuantizer(nn.Module):
         # convert inputs from BCHW -> BHWC
         inputs = inputs.permute(0, 2, 3, 1).contiguous()
         input_shape = inputs.shape
+        batch_size, height, width, channels = input_shape
         # Flatten input
         flat_input = inputs.view(-1, self._embedding_dim)
         # Calculate distances
@@ -40,8 +41,10 @@ class VectorQuantizer(nn.Module):
         quantized = inputs + (quantized - inputs).detach()
         avg_probs = torch.mean(encodings, dim=0)
         perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+        encoding_indices = encoding_indices.view(batch_size, height, width)
         # convert quantized from BHWC -> BCHW
-        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encodings
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encoding_indices
 
 
 class VectorQuantizerEMA(nn.Module):
@@ -87,12 +90,15 @@ class VectorQuantizerEMA(nn.Module):
         # Use EMA to update the embedding vectors
         if self.training:
             self._ema_cluster_size = self._ema_cluster_size * self._decay + (1 - self._decay) * torch.sum(encodings, 0)
+
             # Laplace smoothing of the cluster size
             n = torch.sum(self._ema_cluster_size.data)
             self._ema_cluster_size = (
                     (self._ema_cluster_size + self._epsilon) / (n + self._num_embeddings * self._epsilon) * n)
+
             dw = torch.matmul(encodings.t(), flat_input)
             self._ema_w = nn.Parameter(self._ema_w * self._decay + (1 - self._decay) * dw)
+
             self._embedding.weight = nn.Parameter(self._ema_w / self._ema_cluster_size.unsqueeze(1))
 
         # Loss
@@ -106,7 +112,7 @@ class VectorQuantizerEMA(nn.Module):
 
         encoding_indices = encoding_indices.view(batch_size, height, width)
         # convert quantized from BHWC -> BCHW
-        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encodings
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encoding_indices
 
 
 class Residual(nn.Module):
@@ -114,10 +120,11 @@ class Residual(nn.Module):
         super(Residual, self).__init__()
         self._block = nn.Sequential(nn.ReLU(True),
                                     nn.Conv2d(in_channels=in_channels, out_channels=num_residual_hiddens, kernel_size=3,
-                                              stride=1, padding=1, bias=False),
-                                    nn.ReLU(True),
+                                              stride=1, padding=1),
+                                    nn.InstanceNorm2d(num_residual_hiddens), nn.ReLU(True),
                                     nn.Conv2d(in_channels=num_residual_hiddens, out_channels=num_hiddens, kernel_size=1,
-                                              stride=1, bias=False))
+                                              stride=1),
+                                    nn.InstanceNorm2d(num_residual_hiddens))
 
     def forward(self, x):
         return x + self._block(x)
@@ -137,57 +144,65 @@ class ResidualStack(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
-        super(Encoder, self).__init__()
-        self._conv_1 = nn.Conv2d(in_channels=in_channels, out_channels=num_hiddens // 2, kernel_size=4, stride=2,
-                                 padding=1)
-        self._conv_2 = nn.Conv2d(in_channels=num_hiddens // 2, out_channels=num_hiddens, kernel_size=4, stride=2,
-                                 padding=1)
-        self._conv_3 = nn.Conv2d(in_channels=num_hiddens, out_channels=num_hiddens, kernel_size=3, stride=1, padding=1)
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens, nb_downsample_blocks=2, ):
+        super().__init__()
+        layers = []
+        for i in range(nb_downsample_blocks):
+            layers.append(
+                nn.Conv2d(in_channels=in_channels if i == 0 else num_hiddens, out_channels=num_hiddens, kernel_size=4,
+                          stride=2, padding=1, ))
+            layers.append(nn.InstanceNorm2d(num_hiddens))
+            layers.append(nn.ReLU(True))
+        self.downsample = nn.Sequential(*layers)
+        self._conv = nn.Conv2d(in_channels=num_hiddens, out_channels=num_hiddens, kernel_size=3, stride=1, padding=1)
         self._residual_stack = ResidualStack(in_channels=num_hiddens, num_hiddens=num_hiddens,
                                              num_residual_layers=num_residual_layers,
                                              num_residual_hiddens=num_residual_hiddens)
 
     def forward(self, inputs):
-        x = self._conv_1(inputs)
-        x = F.relu(x)
-        x = self._conv_2(x)
-        x = F.relu(x)
-        x = self._conv_3(x)
+        x = self.downsample(inputs)
+        x = self._conv(x)
         return self._residual_stack(x)
 
 
 class Decoder(nn.Module):
-    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens, nb_upsample_blocks=2,
+                 out_channels=3):
         super(Decoder, self).__init__()
         self._conv_1 = nn.Conv2d(in_channels=in_channels, out_channels=num_hiddens, kernel_size=3, stride=1, padding=1)
         self._residual_stack = ResidualStack(in_channels=num_hiddens, num_hiddens=num_hiddens,
                                              num_residual_layers=num_residual_layers,
                                              num_residual_hiddens=num_residual_hiddens)
-        self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens, out_channels=num_hiddens // 2, kernel_size=4,
-                                                stride=2, padding=1)
-        self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens // 2, out_channels=1, kernel_size=4, stride=2,
-                                                padding=1)
+        layers = []
+        for i in range(nb_upsample_blocks):
+            last = i == nb_upsample_blocks - 1
+            out = out_channels if last else num_hiddens
+            layers.append(
+                nn.ConvTranspose2d(in_channels=num_hiddens, out_channels=out, kernel_size=4, stride=2, padding=1))
+            if not last:
+                layers.append(nn.InstanceNorm2d(num_hiddens))
+                layers.append(nn.ReLU(True))
+        self.upsample = nn.Sequential(*layers)
 
     def forward(self, inputs):
         x = self._conv_1(inputs)
         x = self._residual_stack(x)
-        x = self._conv_trans_1(x)
-        x = F.relu(x)
-        return self._conv_trans_2(x)
+        return self.upsample(x)
 
 
 class VQVAEModel(nn.Module):
-    def __init__(self, num_hiddens, num_residual_layers, num_residual_hiddens, num_embeddings, embedding_dim,
-                 commitment_cost, decay=0):
-        super(VQVAEModel, self).__init__()
-        self._encoder = Encoder(1, num_hiddens, num_residual_layers, num_residual_hiddens)
+    def __init__(self, num_hiddens=128, num_residual_layers=2, num_residual_hiddens=32, num_embeddings=51,
+                 embedding_dim=64, commitment_cost=0.25, decay=0.99, nb_channels=1, nb_blocks=2):
+        super().__init__()
+        self._encoder = Encoder(nb_channels, num_hiddens, num_residual_layers, num_residual_hiddens,
+                                nb_downsample_blocks=nb_blocks)
         self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens, out_channels=embedding_dim, kernel_size=1, stride=1)
         if decay > 0.0:
             self._vq_vae = VectorQuantizerEMA(num_embeddings, embedding_dim, commitment_cost, decay)
         else:
             self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim, commitment_cost)
-        self._decoder = Decoder(embedding_dim, num_hiddens, num_residual_layers, num_residual_hiddens)
+        self._decoder = Decoder(embedding_dim, num_hiddens, num_residual_layers, num_residual_hiddens,
+                                out_channels=nb_channels, nb_upsample_blocks=nb_blocks)
 
     def forward(self, x):
         z = self._encoder(x)
@@ -201,6 +216,7 @@ class VQVAEModel(nn.Module):
         z = self._encoder(x)
         z = self._pre_vq_conv(z)
         loss, quantized, perplexity, encoding_indices = self._vq_vae(z)
+        print('Inside encode in the VQVAE Model: encoding_indices shape='.format(encoding_indices.shape))
         return encoding_indices
 
     def reconstruct_from_code(self, encoding_indices):
